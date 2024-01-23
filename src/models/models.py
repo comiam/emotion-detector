6ba55051de9a13ac4feef0ddc3a84c2280bbb7ff
Maxim Maximov <maxim.bolshim@yandex.ru>
2024-01-24 02:39:57 +0300

    lab 4 improvements

--- src/models/models.py
+++ src/models/models.py
@@ -1,11 +1,11 @@
 from sklearn.linear_model import LogisticRegression
-from sklearn.neural_network import MLPRegressor
+from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 
 
 def get_available_models():
     return [
-        MLPRegressor(hidden_layer_sizes=(100, 200, 200, 150, 100, 50, 1),
+        MLPClassifier(hidden_layer_sizes=(100, 200, 200, 150, 100, 50, 1),
                      max_iter=100000,
                      solver='adam',
                      random_state=42),
